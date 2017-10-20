e2bbc3605aad5c1d384ff2a723de0869a110e95d
tomcat <tomcat.cheng@microlife.com.tw>
2017-10-20 13:47:25 +0800

    init project, add code.

---
+++ app/src/main/java/com/demo/tomcat/notificationiddemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.demo.tomcat.notificationiddemo;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    int notificationID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn_displaynotif);
+        button.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                displayNotification();
+            }
+        });
+    }
+
+    protected void displayNotification()
+    {
+        Intent i = new Intent(this, NotificationView.class);
+        i.putExtra("notificationID", notificationID);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        Notification notif = new Notification(
+                R.drawable.ic_launcher_foreground,
+                "Reminder: Meeting starts in 5 minutes",
+                System.currentTimeMillis());
+
+        CharSequence from = "System Alarm";
+        CharSequence message = "Meeting with customer at 3pm...";
+
+        notif.setLatestEventInfo(this, from, message, pendingIntent);
+
+        notif.vibrate = new long[] { 100, 250, 100, 500};
+        nm.notify(notificationID, notif);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/demo/tomcat/notificationiddemo/NotificationView.java
@@ -0,0 +1,17 @@
+package com.demo.tomcat.notificationiddemo;
+
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        nm.cancel(getIntent().getExtras().getInt("notificationID"));
+    }
+}
